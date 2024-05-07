acfa5d5f26d0e1eae0dafbff675fe6866e5164f9
Felipe Morales <fmoralesch@outlook.com>
2024-05-07 12:25:50 -0400

    added gitignore - added docstring for a few solutions

--- top-interview-150/find-index-first-occurence.py
+++ top-interview-150/find-index-first-occurence.py
@@ -1,17 +1,38 @@
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        for i in range(len(haystack)):
-            if haystack[i] == needle[0]:
-                if haystack[i:len(needle) + i] == needle:
-                    return i
-        return -1
+
+def strStr(haystack: str, needle: str) -> int:
+    """Given two strings needle and haystack, 
+    return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+    Example 1:
+
+    Input: haystack = "sadbutsad", needle = "sad"
+    Output: 0
+    Explanation: "sad" occurs at index 0 and 6.
+    The first occurrence is at index 0, so we return 0.
+    Example 2:
+
+    Input: haystack = "leetcode", needle = "leeto"
+    Output: -1
+    Explanation: "leeto" did not occur in "leetcode", so we return -1.
+    
+    Examples
+    -------
+    >>> strStr("sadbutsad", "sad")
+    0
+    >>> strStr("leetcode", "leeto")
+    -1
+    """
+    for i in range(len(haystack)):
+        if haystack[i] == needle[0]:
+            if haystack[i:len(needle) + i] == needle:
+                return i
+    return -1
 
 if __name__ == '__main__':
-    sol = Solution()
     inp1 = "sadbutsad"
     inp2 = "sad"
-    assert sol.strStr(inp1, inp2) == 0
+    assert strStr(inp1, inp2) == 0
 
     inp1 = "leetcode"
     inp2 = "leeto"
-    assert sol.strStr(inp1, inp2) == -1
+    assert strStr(inp1, inp2) == -1
--- top-interview-150/length-of-last-word.py
+++ top-interview-150/length-of-last-word.py
@@ -1,15 +1,35 @@
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        words = [string for string in s.split(' ') if string != '']
-        return len(words[len(words) -1 :][0])
+def lengthOfLastWord(s: str) -> int:
+    """Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+    A word is a maximal 
+    substring
+    consisting of non-space characters only.
+
+    Example 1:
+
+    Input: s = "   fly me   to   the moon  "
+    Output: 4
+    Explanation: The last word is "moon" with length 4.
+    
+    Examples
+    -------
+    >>> lengthOfLastWord("Hello World")
+    5
+    >>> lengthOfLastWord("   fly me   to   the moon  ")
+    4
+    >>> lengthOfLastWord("luffy is still joyboy")
+    6
+    """
+    words = [string for string in s.split(' ') if string != '']
+    return len(words[-1])
+
 
 if __name__ == '__main__':
-    sol = Solution()
     inp = "Hello World"
-    assert sol.lengthOfLastWord(inp) == 5
+    assert lengthOfLastWord(inp) == 5
 
     inp = "   fly me   to   the moon  "
-    assert sol.lengthOfLastWord(inp) == 4
+    assert lengthOfLastWord(inp) == 4
 
     inp = "luffy is still joyboy"
-    assert sol.lengthOfLastWord(inp) == 6
+    assert lengthOfLastWord(inp) == 6
