5c6107577843f57f5fd3571a63d07de6b87f16bd
Felipe Morales <fmoralesch@outlook.com>
2024-05-07 12:36:00 -0400

    adding docstring for majority element solution, and convert the implementation as functional

--- top-interview-150/majority-element.py
+++ top-interview-150/majority-element.py
@@ -1,24 +1,39 @@
 from typing import List
 
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        major = nums[0]
-        majority = len(nums) / 2
-        freqDict = {}
-        for num in nums:
-            freq = freqDict.get(num, 0)
-            freq += 1
-            freqDict[num] = freq
-        
-        for value, freq in freqDict.items():
-            if freq > majority:
-                return value
-        return major
+def majorityElement(nums: List[int]) -> int:
+    """Given an array nums of size n, return the majority element.
+
+    The majority element is the element that appears more than ⌊n / 2⌋ times. 
+    You may assume that the majority element always exists in the array.
+
+    Example 1:
+
+    Input: nums = [2,2,1,1,1,2,2]
+    Output: 2
+    
+    Examples
+    -------
+    >>> majorityElement([3,2,3])
+    3
+    >>> majorityElement([2,2,1,1,1,2,2])
+    2
+    """
+    major = nums[0]
+    majority = len(nums) / 2
+    freqDict = {}
+    for num in nums:
+        freq = freqDict.get(num, 0)
+        freq += 1
+        freqDict[num] = freq
+    
+    for value, freq in freqDict.items():
+        if freq > majority:
+            return value
+    return major
 
 
 if __name__ == '__main__':
-    sol = Solution()
     inp = [3,2,3]
-    assert sol.majorityElement(inp) == 3
+    assert majorityElement(inp) == 3
     inp = [2,2,1,1,1,2,2]
-    assert sol.majorityElement(inp) == 2
+    assert majorityElement(inp) == 2
